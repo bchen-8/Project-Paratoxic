3abe2a9e43ca22b43d7fcf0bc46185700972a2a8
SJCRPV <sjcrpv.games@hotmail.com>
2020-05-08 22:21:46 +0100

    Blocking user input during dream sequence until text is fully written out

---
+++ Assets/Code/BlockInputUntilTextIsWrittenOut.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockInputUntilTextIsWrittenOut : MonoBehaviour
+{
+    private GameManager gameManager;
+    private EventManager eventManager;
+    private Transform transformToObserve;
+    private int previousChildCount;
+    private int previousControlModeId;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventManager = GameObject.Find("Overlord").GetComponent<EventManager>();
+        gameManager = GameObject.Find("Overlord").GetComponent<GameManager>();
+        previousControlModeId = gameManager.controlMode;
+        transformToObserve = GameObject.Find("Phone").transform;
+        previousChildCount = transformToObserve.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameManager.playingDialogue)
+        {
+            eventManager.ControlMode(2);
+        }
+        else
+        {
+            eventManager.ControlMode(0);
+        }
+        //if(transformToObserve.childCount > previousChildCount)
+        //{
+        //    eventManager.ControlMode(2);
+        //    if(!gameManager.playingDialogue)
+        //    {
+        //        eventManager.ControlMode(previousControlModeId);
+        //        previousChildCount = transformToObserve.childCount;
+        //    }
+        //}
+    }
+}
